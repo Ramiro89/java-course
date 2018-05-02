2df9ae669ceaa2f23af93cfafef11cfa05464f95
Ramiro89 <37081238+Ramiro89@users.noreply.github.com>
2018-05-02 17:33:36 -0300

    resolviendo errores de compilacion

--- src/mundopc/MundoPC.java
+++ src/mundopc/MundoPC.java
@@ -5,7 +5,7 @@ public class MundoPC {
 public static void main(String [] arg){
     //Creacion de computadora Toshiba
     Monitor monitorToshi=new Monitor("Toshiba", 17);
-    Teclado tecladoTo
+    Teclado tecladoTo;
 }
 
 }
